16e13b9586bf1028f9d6e68a2cd6b91f251e3400
upstreamlab@myhelaina.com <upstreamlab@myhelaina.com>
2023-11-21 22:08:23 -0500

    Moved passwords to .env

--- odbc_test.R
+++ odbc_test.R
@@ -1,11 +1,10 @@
-install.packages(c("DBI", "dplyr","dbplyr","odbc"))
 library(DBI)
 library(dplyr)
 library(dbplyr)
 library(odbc)
 
 
-myconn <- dbConnect(odbc::odbc(), "shashwat", pwd='Shyam_Sundar_Tewari31!', role = 'ACCOUNTADMIN')
+myconn <- dbConnect(odbc::odbc(), "shashwat", pwd= Sys.getenv('SNOWFLAKE_PASSWORD'), role = 'ACCOUNTADMIN')
 
 DBI::dbGetQuery(myconn,"USE WAREHOUSE USP;") 
 DBI::dbGetQuery(myconn,"USE DATABASE USP_DATA;") 
